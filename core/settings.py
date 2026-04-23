76ef86bd63f957619dc07b99b78c7248e92ddeff
Juan Ramirez <jcramirezlopez06@gmail.com>
2026-04-22 22:49:11 -0500

    add geographical libraries into the project

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "django_countries",
+    "localflavor",
     'crispy_forms',
     'crispy_bootstrap5',
     'accounts',
